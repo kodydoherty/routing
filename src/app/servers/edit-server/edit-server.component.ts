e05f1135a5e9ed5eaf5f4462405e17e69cafe486
Kody Doherty <kodydoherty@gmail.com>
2017-10-01 13:05:22 -0400

    stop people from leaving with out confirm alert

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import { Observable } from 'rxjs/Observable';
 import { ServersService } from '../servers.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-edit-server',
@@ -13,8 +15,11 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute,
+              private router: Router  ) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
@@ -34,6 +39,19 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route });
+  }
+
+  canDeactivate():  Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status ) && !this.changesSaved) {
+      return confirm('Do you want to discard the changes?')
+    } else {
+      return true;
+    }
   }
 
 }
